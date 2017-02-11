014e0bbe78353546c168c2a6449b51b53f59b7c7
Morgane Troysi <morgane.troysi@etud.univ-angers.fr>
2017-02-11 19:33:48 +0100

    Page de contact - ajout msg flash trop cool

--- mySweetCompany/views.py
+++ mySweetCompany/views.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 from django.shortcuts import render
 from django.conf import settings
 from produits.models import Produit
@@ -6,6 +7,7 @@ from django.core.mail import EmailMessage
 from django.shortcuts import redirect
 from django.template import Context
 from django.template.loader import get_template
+from django.contrib import messages
 
 
 # def home(request):
@@ -53,22 +55,23 @@ def contact(request):
             # Email the profile with the
             # contact information
             template = get_template('contact_template.txt')
-        context = Context({
-            'contact_name': contact_name,
-            'contact_email': contact_email,
-            'form_content': form_content,
-        })
-        content = template.render(context)
+            context = Context({
+                'contact_name': contact_name,
+                'contact_email': contact_email,
+                'form_content': form_content,
+            })
+            content = template.render(context)
 
-        email = EmailMessage(
-            "New contact form submission",
-            content,
-            "Your website" + '',
-            ['youremail@gmail.com'],
-            headers={'Reply-To': contact_email}
-        )
-        email.send()
-        return redirect('contact')
+            email = EmailMessage(
+                "New contact form submission",
+                content,
+                "Your website" + '',
+                ['youremail@gmail.com'],
+                headers={'Reply-To': contact_email}
+            )
+            email.send()
+            messages.add_message(request, messages.INFO, 'Message envoyé.')
+            return redirect('contact')
     return render(request, 'contact.html', {'form': form_class})
 
 
