2aefc7e080019ea3140f78036afde465a81ffc24
Morgane Troysi <morgane.troysi@etud.univ-angers.fr>
2017-02-11 18:31:39 +0100

    Liste des produits

--- mySweetCompany/views.py
+++ mySweetCompany/views.py
@@ -1,33 +1,38 @@
 from django.shortcuts import render
 from django.conf import settings
-from client.models import Client
+from produits.models import Produit
 
 # def home(request):
 #     name='Valou'
 #     n=36
 #     return render(request, 'base.html', {'name':name, 'n':n})
 
-def home(request):
-    return render(request, 'home.html')
 
-# def clients(request):
-#     clients = Client.objects.all()
-#     return render(request, 'clients.html', {'clients':clients})
+def home(request):
+    produits = Produit.objects.all().order_by('-date_publication')[:10]
+    return render(request, 'home.html', {'produits':produits})
+
 
 def login(request):
     return render(request, 'connexion.html')
 
+
 def inscription(request):
     return render(request, 'inscription.html')
 
+
 def produits(request):
-    return render(request, 'produits.html')
+    produits = Produit.objects.all()
+    return render(request, 'produits.html', {'produits':produits})
+
 
 def panier(request):
     return render(request, 'panier.html')
 
+
 def contact(request):
     return render(request, 'contact.html')
+
 
 def credits(request):
     return render(request, 'credits.html')
