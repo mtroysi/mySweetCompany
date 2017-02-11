e71a42fe5f0d7c664ae394c1f2e70ebe42115e56
Morgane Troysi <morgane.troysi@etud.univ-angers.fr>
2017-02-11 18:59:36 +0100

    Page de contact

--- mySweetCompany/views.py
+++ mySweetCompany/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
 from django.conf import settings
 from produits.models import Produit
+from mySweetCompany.forms import ContactForm
+from django.core.mail import EmailMessage
+from django.shortcuts import redirect
+from django.template import Context
+from django.template.loader import get_template
+
 
 # def home(request):
 #     name='Valou'
@@ -10,7 +16,7 @@ from produits.models import Produit
 
 def home(request):
     produits = Produit.objects.all().order_by('-date_publication')[:10]
-    return render(request, 'home.html', {'produits':produits})
+    return render(request, 'home.html', {'produits': produits})
 
 
 def login(request):
@@ -23,7 +29,7 @@ def inscription(request):
 
 def produits(request):
     produits = Produit.objects.all()
-    return render(request, 'produits.html', {'produits':produits})
+    return render(request, 'produits.html', {'produits': produits})
 
 
 def panier(request):
@@ -31,7 +37,39 @@ def panier(request):
 
 
 def contact(request):
-    return render(request, 'contact.html')
+    form_class = ContactForm
+    if request.method == 'POST':
+        form = form_class(data=request.POST)
+
+        if form.is_valid():
+            contact_name = request.POST.get(
+                'contact_name'
+                , '')
+            contact_email = request.POST.get(
+                'contact_email'
+                , '')
+            form_content = request.POST.get('content', '')
+
+            # Email the profile with the
+            # contact information
+            template = get_template('contact_template.txt')
+        context = Context({
+            'contact_name': contact_name,
+            'contact_email': contact_email,
+            'form_content': form_content,
+        })
+        content = template.render(context)
+
+        email = EmailMessage(
+            "New contact form submission",
+            content,
+            "Your website" + '',
+            ['youremail@gmail.com'],
+            headers={'Reply-To': contact_email}
+        )
+        email.send()
+        return redirect('contact')
+    return render(request, 'contact.html', {'form': form_class})
 
 
 def credits(request):
