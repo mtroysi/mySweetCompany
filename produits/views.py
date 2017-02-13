027a4403d648353b23cc27340684644c824335c3
Morgane Troysi <morgane.troysi@etud.univ-angers.fr>
2017-02-13 22:43:46 +0100

    Création routing vue individuelle produit

--- produits/views.py
+++ produits/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from produits.models import Produit
 
 # Create your views here.
+
+
+def show(request, id):
+    produit = Produit.objects.get(id=id)
+    return render(request, 'show.html', {'produit':produit})
